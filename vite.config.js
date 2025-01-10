e1052e367df62ec3d056701f7d6ddd64cf786095
bontusalih <bontusalih1@gmail.com>
2025-01-10 02:32:01 +0200

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/yourstech.net/',
-  plugins: [react()],
-  assetsInclude: ['**/*.MP4', '**/*.mp4'],
-})
